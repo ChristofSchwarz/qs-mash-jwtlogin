5925bed53e5727931741246d1d8131554cacf741
Christof Schwarz <christof.schwarz@databridge.ch>
2022-06-19 17:08:45 +0000

    Add files via upload

--- node_createtokens/createtoken.js
+++ node_createtokens/createtoken.js
@@ -1,19 +1,19 @@
-var userDir = process.argv[2];
+var directory = process.argv[2];
 var user = process.argv[3];
 var pass = process.argv[4];
 const defaultCertPath = 'C:\\ProgramData\\Qlik\\Sense\\Repository\\Exported Certificates\\.Local Certificates';
 
-if (user == undefined || pass == undefined || userDir == undefined) {
-    console.log('This .js requires 3 parameters: [userDir] [user] [password] as command-line arguments.');
+if (user == undefined || pass == undefined || directory == undefined) {
+    console.log('This .js requires 3 parameters: [directory] [user] [password] as command-line arguments.');
 } else {
-    console.log(`Creating token for user "${userDir}\\${user}", encrypted with password "${pass}"`);
+    console.log(`Creating token for user "${directory}\\${user}", encrypted with password "${pass}"`);
     var jwt = require('jsonwebtoken');
     var fs = require('fs');
     var crypto = require('crypto-js');
-    var privateKeyFile = fs.existsSync('./client_key.pem')?`./client_key.pem`:`${defaultCertPath}\\client_key.pem`;
+    var privateKeyFile = fs.existsSync('./private_key.pem')?`./private_key.pem`:`${defaultCertPath}\\client_key.pem`;
     console.log(`Using private key "${privateKeyFile}"`);
     var privateKey = fs.readFileSync(privateKeyFile);
-    var token = jwt.sign({ UserId: user.toLowerCase(), UserDirectory: userDir.toUpperCase() }, privateKey, { algorithm: 'RS512'});
+    var token = jwt.sign({ user: user.toLowerCase(), directory: directory.toUpperCase() }, privateKey, { algorithm: 'RS512'});
     console.log('');
     console.log('JWT token below:');
     console.log('');
@@ -21,7 +21,7 @@ if (user == undefined || pass == undefined || userDir == undefined) {
     var encryptedToken = crypto.AES.encrypt(token, pass);
     var encryptedUser = crypto.MD5(user);
     console.log('');
-    console.log('Add this to config.json:');
+    console.log('Add this to config.json (0 is the index of redir_targets in config.json and can be changed):');
     console.log('');
-    console.log(`,"${encryptedUser}": "${encryptedToken}"`);
+    console.log(`,"${encryptedUser}": [0, "${encryptedToken}"]`);
 }
